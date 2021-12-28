cdf35397e17f55d514ffce08838f188f1324c973
elther <elther@naver.com>
2021-12-29 01:39:00 +0900

    refactor : Ant design - know using antd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 // Webpack CSS import
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
-import OnsenuiExample from './Components/Onsenui/OnsenuiExample';
+// import 'onsenui/css/onsenui.css';
+// import 'onsenui/css/onsen-css-components.css';
+// import OnsenuiExample from './Components/Onsenui/OnsenuiExample';
+import AntDesignExample from './Components/AntDesign/AntDesignExample';
 
 function App() {
   return (
-    
-      <OnsenuiExample />
-    
+    <AntDesignExample />
+      // <OnsenuiExample />
   );
 }
 
---
+++ src/Components/AntDesign/IconExample.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { CameraOutlined, SearchOutlined } from '@ant-design/icons/lib/icons'
+import { Button, Tooltip } from 'antd'
+
+export default function IconExample() {
+    return (
+        <div>
+            <CameraOutlined />
+            <Tooltip title="search">
+            <Button size="large" type="primary" shape="circle" icon={<SearchOutlined />} />
+            </Tooltip>
+            <Button type="primary" shape="circle">
+            A
+            </Button>
+            <Button type="primary" icon={<SearchOutlined />} loading={true}>
+            Search
+            </Button>
+            <Tooltip title="search">
+            <Button shape="circle" icon={<SearchOutlined />}/>
+            </Tooltip>
+            <Button icon={<SearchOutlined />}>Search</Button>
+            <br />
+            <Tooltip title="search">
+            <Button shape="circle" icon={<SearchOutlined />} />
+            </Tooltip>
+            <Button icon={<SearchOutlined />}>Search</Button>
+            <Tooltip title="search">
+            <Button type="dashed" shape="circle" icon={<SearchOutlined />} />
+            </Tooltip>
+            <Button type="dashed" icon={<SearchOutlined />}>
+            Search
+            </Button>
+            <Button icon={<SearchOutlined />} href="https://www.google.com" />         
+        </div>
+    )
+}
+
