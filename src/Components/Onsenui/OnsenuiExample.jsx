9dd83185016bcace65c16a49808d3e776d805ed8
elther <elther@naver.com>
2021-12-29 00:54:49 +0900

    refactor : onsenui - know using onsenui

--- src/Components/Onsenui/OnsenuiExample.jsx
+++ src/Components/Onsenui/OnsenuiExample.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import ButtonExample from './ButtonExample'
 import { Page } from 'react-onsenui'
 import ActionSheetExample from './ActionSheetExample'
+import CheckBoxExample from './CheckBoxExample'
+import FabExample from './FabExample'
+import ListExample from './ListExample'
+import ProgressExample from './ProgressExample'
+import TabExample from './TabExample'
+import AlertExample from './AlertExample'
 
 export default function OnsenuiExample() {
     return (
         <Page>
+            {/* <TabExample /> */}
+            <ProgressExample />
+            <ListExample />
+            <FabExample />
+            <CheckBoxExample />
             <ActionSheetExample />
             <ButtonExample />
+            <AlertExample />
         </Page>
     )
 }
