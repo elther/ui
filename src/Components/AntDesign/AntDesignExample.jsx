40a650ead4186665f99eabfbd0a17c2bfd62d74e
elther <elther@naver.com>
2021-12-29 16:03:51 +0900

    refactor : Ant Design - Form, Layout, Modal, Table

--- src/Components/AntDesign/AntDesignExample.jsx
+++ src/Components/AntDesign/AntDesignExample.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import FormExample from './FormExample';
 import GetStart from './GetStart';
 import IconExample from './IconExample';
+import LayoutExample from './LayoutExample';
+import ModalExample from './ModalExample';
+import TableExample from './TableExample';
 import TypographyExample from './TypographyExample';
 
 export default function AntDesignExample() {    
     return (
       <div>
+          <FormExample />
+          <TableExample />
+          {/* <LayoutExample /> */}
+          {/* <ModalExample />
           <TypographyExample />
           <IconExample />
-          <GetStart />
+          <GetStart /> */}
       </div>
     );
 }
